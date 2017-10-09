49aa737dc856c0f949badca718c80f6309a90558
tenination <tim.ninan@gmail.com>
2017-10-09 12:34:45 -0500

    finish all bee classes

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing";
+
 };
+
+  //do more research on why the following two lines are needed
+  Bee.prototype = Object.create(Grub.prototype);
+  Bee.prototype.constructor = Bee; 
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,23 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot = 0;
+  
+  
 };
 
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+  //console.log(this);
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
